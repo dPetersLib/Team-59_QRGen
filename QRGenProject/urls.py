fa88d537c44e7feed14290a7802ba5b768c8f75f
edoka <peter.edoka.242464@unn.edu.ng>
2023-01-20 11:02:29 +0100

    feat: poetry setup

--- QRGenProject/urls.py
+++ QRGenProject/urls.py
@@ -1,7 +1,7 @@
 """QRGenProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -26,7 +26,7 @@ urlpatterns = [
     path('qrcode/', include('handlescan.urls')),
 ] 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404 = "home.views.not_found"
