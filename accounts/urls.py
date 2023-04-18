a2f0e281a7babbfd3058b6c15c55c09a47c71e00
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-18 19:09:28 +0100

    fix: added admin register functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Register, Login, logoutuser
+from .views import Register, Login, logoutuser, adminReg
 app_name = "accounts"
 
 
 urlpatterns = [
     path('register/', Register, name="register-page"),
+    path('register/admin/', adminReg, name="adminreg"),
     path('login/', Login, name="login-page"),
     path('logout/', logoutuser, name="logout" )
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,34 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from QRGenProject.settings import ADMIN_AUTH_TOKEN
+
+
 # Create your views here.
 
+def adminReg(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('pswrd')
+        uname = request.POST.get('email')
+        auth_token = request.POST.get('authtoken')
+        if auth_token != ADMIN_AUTH_TOKEN:
+            messages.error(
+                request, 'Invalid Authorization!')
+        elif User.objects.filter(username=uname).first():
+            messages.error(
+                request, 'Email address already exist, register a new email address!')
+        else:
+            new_user = User.objects.create_user(
+                username=uname,
+                email=email,
+                password=password,
+                is_staff=True,
+                is_superuser=True
+            )
+            login(request, new_user)
+            return redirect('qrgen:dashboard')
+    return render(request, 'accounts/register.html', {"adminreg": True})
 
 def Register(request):
     if request.user.is_authenticated:
